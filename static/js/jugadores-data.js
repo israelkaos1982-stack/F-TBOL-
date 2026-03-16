7ff7c18e5e9456e97fc8696932c84cdaf157af2c
israelkaos1982-stack <israelkaos1982@hotmail.com>
2026-03-16 18:10:00 +0100

    Update jugadores-data.js

--- static/js/jugadores-data.js
+++ static/js/jugadores-data.js
@@ -1,24 +1,116 @@
+/**
+ * BASE DE DATOS EXTENSA DE JUGADORES
+ * Ubicación: static/js/jugadores-data.js
+ */
+
 const JUGADORES_DATA = {
-    "Real Madrid": ["Kylian Mbappé", "Vinicius Júnior", "Rodrygo", "Jude Bellingham", "Federico Valverde"],
-    "FC Barcelona": ["Robert Lewandowski", "Lamine Yamal", "Raphinha", "Pedri", "Gavi"],
-    "Atletico Madrid": ["Julián Alvarez", "Antoine Griezmann", "Alexander Sørloth", "Marcos Llorente"],
-    "Athletic Club": ["Nico Williams", "Iñaki Williams", "Gorka Guruzeta", "Oihan Sancet"],
-    "Villarreal": ["Gerard Moreno", "Ayoze Pérez", "Nicolas Pépé"],
-    "Betis": ["Cucho Hernández", "Isco", "Giovani Lo Celso", "Abde"],
-    "Osasuna": ["Ante Budimir", "Bryan Zaragoza", "Aimar Oroz"],
-    "Real Sociedad": ["Mikel Oyarzabal", "Takefusa Kubo", "Brais Méndez"],
-    "Girona": ["Cristhian Stuani", "Bryan Gil", "Viktor Tsygankov"],
-    "Rayo Vallecano": ["James Rodríguez", "Isi Palazón", "Sergio Camello"],
-    "Espanyol": ["Javi Puado", "Alejo Véliz", "Cardona"],
-    "Celta": ["Iago Aspas", "Borja Iglesias", "Williot Swedberg"],
-    "Mallorca": ["Vedat Muriqi", "Abdon Prats", "Cyle Larin"],
-    "Valencia": ["Hugo Duro", "Diego López", "Pepelu"],
-    "Sevilla": ["Isaac Romero", "Lukebakio", "Suso"],
-    "Getafe": ["Borja Mayoral", "Carles Pérez", "Uche"],
-    "Deportivo Alavés": ["Toni Martínez", "Kike García", "Carlos Vicente"],
-    "Elche": ["Sory Kaba", "Oscar Plano", "Nico Castro"],
-    "Real Oviedo": ["Alemao", "Santi Cazorla", "Ilyas Chaira"],
-    "Levante": ["José Luis Morales", "Carlos Álvarez", "Brugué"]
+    "Real Madrid": [
+        "Courtois", "Lunin", "Fran González", "Mestre",
+        "Trent", "Carvajal", "Rüdiger", "Militão", "Huijsen", "Alaba", "Mendy", "Carreras", "Asencio", "Fran García",
+        "Bellingham", "Valverde", "Tchouaméni", "Camavinga", "Arda Güler", "Brahim", "Ceballos", "Gonzalo", "Mastantuono",
+        "Mbappé", "Vinicius", "Rodrygo"
+    ],
+    "FC Barcelona": [
+        "Joan García", "Szczęsny", "Kochen",
+        "Koundé", "Cancelo", "Balde", "Eric García", "Araújo", "Cubarsí", "G. Martín", "Christensen",
+        "Pedri", "F. de Jong", "Dani Olmo", "Gavi", "Fermín", "Rashford", "Casadó", "Bernal",
+        "Lamine Yamal", "Raphinha", "Lewandowski", "Ferran Torres", "Bardghji"
+    ],
+    "Atlético Madrid": [
+        "Oblak", "Musso", "Esquivel",
+        "Marcos Llorente", "Hancko", "Giménez", "Le Normand", "Lenglet", "Pubill", "N. Molina", "Ruggeri",
+        "Griezmann", "P. Barrios", "Koke", "Cardoso", "G. Simeone", "De Paul", "Almada",
+        "Julián Álvarez", "Sørloth", "Lookman", "Álex Baena", "N. González"
+    ],
+    "Athletic Club": [
+        "Unai Simón", "Padilla", "Manex",
+        "Laporte", "Vivian", "Yeray", "Berchiche", "Gorosabel", "Areso", "Lekue", "Paredes",
+        "Sancet", "B. Prados", "Galarreta", "Jauregizar", "Vesga", "U. Gómez",
+        "Nico Williams", "Iñaki Williams", "Berenguer", "Guruzeta"
+    ],
+    "Villarreal": [
+        "Luís Júnior", "Tenas", "Conde",
+        "R. Veiga", "Cardona", "Pau Navarro", "Pedraza", "Logan Costa", "Mouriño", "Rafa Marín", "Foyth",
+        "P. Gueye", "Comesaña", "Moleiro", "Buchanan", "Partey",
+        "Gerard Moreno", "Ayoze", "Pépé", "Mikautadze"
+    ],
+    "Real Sociedad": [
+        "Remiro", "U. Marrero",
+        "Zubeldia", "Jon Martín", "Elustondo", "Odriozola", "Jon Aramburu", "S. Gómez",
+        "Brais Méndez", "Kubo", "Zakharyan", "Y. Herrera", "Turrientes", "Guedes", "C. Soler", "Sučić",
+        "Oyarzabal", "Barrenetxea", "Óskarsson"
+    ],
+    "Betis": [
+        "Valles", "Pau López", "Adrián",
+        "Bartra", "Natan", "Ruibal", "Llorente", "Junior Firpo",
+        "Isco", "Lo Celso", "Fornals", "Antony", "Fidalgo", "M. Roca", "Amrabat", "Deossa",
+        "Cucho", "Abde", "V. Gómez"
+    ],
+    "Sevilla": [
+        "Vlachodimos", "Nyland",
+        "Azpilicueta", "Gudelj", "Babá Mendy", "Kike Salas", "Suazo", "Juanlu", "Marcão",
+        "Agoumé", "D. Sow", "Joan Jordán", "Cardoso", "Januzaj", "Peque", "Carmona",
+        "R. Vargas", "Ejuke", "Á. Sánchez", "Maupay", "Isaac Romero"
+    ],
+    "Girona": [
+        "Gazzaniga", "R. Blanco",
+        "A. Moreno", "V. Reis", "Blind", "A. Martínez", "Witsel", "Francés", "D. López",
+        "Lemar", "Bryan Gil", "Beltrán", "I. Martín", "Tsygankov", "Echeverri", "Ounahi", "Van de Beek", "Portu",
+        "Vanat", "Stuani"
+    ],
+    "Osasuna": [
+        "S. Herrera", "A. Fernández",
+        "Boyomo", "Catena", "Rosier", "Galán", "Bretones", "J. Cruz", "Herrando",
+        "V. Muñoz", "R. García", "R. Moro", "Moncayola", "Torró", "Í. Muñoz", "Aimar Oroz", "M. Gómez",
+        "Budimir", "Arnau"
+    ],
+    "Valencia": [
+        "Dimitrievski", "Agirrezabala",
+        "Gayà", "Copete", "Tárrega", "T. Correia", "U. Sadiq", "J. Vázquez", "Cömert", "Diakhaby",
+        "Pepelu", "Ugrinič", "D. López", "L. Beltrán", "Luis Rioja", "Javi Guerra", "G. Rodríguez", "Daniuma", "A. Almeida",
+        "Hugo Duro"
+    ],
+    "Celta": [
+        "Iván Villar", "Andrei Radu",
+        "Á. Núñez", "Jutglà", "Aidoo", "Carreira", "Marcos Alonso", "Starfelt", "J. Rodríguez",
+        "H. Álvarez", "Vecino", "Sotelo", "Mingueza", "Moriba",
+        "Swedberg", "Borja Iglesias", "Iago Aspas"
+    ],
+    "Espanyol": [
+        "Dmitrović", "Fortuño",
+        "L. Cabrera", "C. Romero", "El Hilali", "Calero", "Riedel",
+        "Edu Expósito", "P. Lozano", "P. Milla", "U. González", "T. Dolan", "Terrats", "Puado",
+        "R. Fernández"
+    ],
+    "Rayo Vallecano": [
+        "Batalla", "Cárdenas",
+        "Chavarría", "N. Mendy", "Lejeune", "Ratiu", "Balliu", "Mumín",
+        "U. López", "P. Ciss", "Á. García", "Isi", "Akhomach", "Ó. Valentín",
+        "D. Frutos", "Alemão", "Camello"
+    ],
+    "Getafe": [
+        "David Soria", "Letáček",
+        "Djené", "D. Duarte", "D. Rico", "Abqar", "J. Iglesias", "Mayoral",
+        "Arambarri", "Luis Milla", "J. Muñoz", "Sancris", "Birmančević",
+        "Satriano", "Juanmi"
+    ],
+    "Real Oviedo": [
+        "Escandell", "Moldovan",
+        "N. Vidal", "J. López", "D. Carmo", "Costas", "Bailly", "D. Calvo",
+        "Colombatto", "Sibo", "A. Reina", "Chairá", "H. Hassan", "Cazorla", "Dendoncker",
+        "F. Viñas"
+    ],
+    "Levante": [
+        "M. Ryan", "Campos",
+        "Toljan", "M. Sánchez", "Dela", "Morales", "Brugui",
+        "P. Martínez", "C. Álvarez", "I. Romero", "Arriaga", "Olasagasti", "Vencedor",
+        "K. Edouard"
+    ]
 };
 
 window.JUGADORES_DATA = JUGADORES_DATA;
+
+// Exportación para Node si fuera necesario
+if (typeof module !== 'undefined') {
+    module.exports = JUGADORES_DATA;
+}
